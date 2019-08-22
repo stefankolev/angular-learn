cc310ce1843b425353612aa26c68608b3b0e7b35
Stefan Kolev <stefankolev@icloud.com>
2019-08-22 20:55:28 +0200

    refac

--- shopping-list/src/environments/environment.ts
+++ shopping-list/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebaseAPIKey: 'AIzaSyB7TluGRPu7p69x-asCkvVMFrahsp-anHc'
+
 };
 
 /*
