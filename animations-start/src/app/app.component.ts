add6604c7bdfdd39ae824874f3d9cf1a56b41085
Stefan Kolev <stefankolev@icloud.com>
2019-08-25 21:46:56 +0200

    adsf

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes, group } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -98,10 +98,17 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
         ]))
       ]), 
       transition('* => void', [
-        animate(300, style({
-          transform: 'translateX(100px)', 
-          opacity: 0
-        }))
+        group([
+          animate(300, style({
+            color: 'red'
+          })), 
+          animate(800, style({
+            transform: 'translateX(100px)', 
+            opacity: 0
+          })), 
+        ]),
+       
+
       ])
       // transition('highlighted => normal', animate(800))
 
@@ -128,6 +135,13 @@ export class AppComponent {
 
     onDelete(item) { 
       this.list.splice(this.list.indexOf(item), 1);
+    }
+
+    animationStarted(event) { 
+      console.log(event);
+    }
+    animationEnded(event) { 
+      console.log(event);
     }
 
 }
